3f9cd4c9578778f3e401493a9fbec114e72d06b4
Gurchetan Singh <gurchetan.singh2@icloud.com>
2026-07-28 15:31:20 +0530

    FIX:useState hooks to add and remove correct platforms on checkbox change

--- postcomposer/src/components/postcomposer.jsx
+++ postcomposer/src/components/postcomposer.jsx
@@ -6,14 +6,32 @@ function PostComposer(){
         <div>
             <h1>PostComposer</h1>
             <p>Choose social media platform where u wish to post:</p>
-            <form onClick={(e)=> setSelectedPlatforms([...selectedPlatforms,e.target.name])}>
+            <form>
                 <div className="platforms">
                     <label>X(formely twitter)</label>
-                    <input type="checkbox" id="twitter" name="twitter"></input>
+                    <input type="checkbox" id="twitter" name="twitter" onChange={(e)=>{
+                        if(e.target.checked){
+                            setSelectedPlatforms([...selectedPlatforms, e.target.name])
+                        }else{
+                            setSelectedPlatforms(selectedPlatforms.filter((selectedPlatform)=>selectedPlatform!=e.target.name))
+                        }
+                        }}></input>
                     <label>Instagram</label>
-                    <input type="checkbox" id="instagram" name="instagram"></input>
+                    <input type="checkbox" id="instagram" name="instagram" onChange={(e)=>{
+                        if(e.target.checked){
+                            setSelectedPlatforms([...selectedPlatforms, e.target.name])
+                        }else{
+                            setSelectedPlatforms(selectedPlatforms.filter((selectedPlatform)=>selectedPlatform!=e.target.name))
+                        }
+                        }}></input>
                     <label>Facebook</label>
-                    <input type="checkbox" id="Facebook" name="Facebook"></input>
+                    <input type="checkbox" id="Facebook" name="Facebook" onChange={(e)=>{
+                        if(e.target.checked){
+                            setSelectedPlatforms([...selectedPlatforms, e.target.name])
+                        }else{
+                            setSelectedPlatforms(selectedPlatforms.filter((selectedPlatform)=>selectedPlatform!=e.target.name))
+                        }
+                        }}></input>
                 </div>
                 <div className="userPost">
                         <textarea value={postText} onChange={(e)=> setPostText(e.target.value)}></textarea>
