369fcbd0f66f5f89a638852282dcf6fec6ebdf8f
Gurchetan Singh <gurchetan.singh2@icloud.com>
2026-07-30 13:49:07 +0530

    ADD: Conditional rendering for textarea borders and live character counter colors

--- postcomposer/src/components/postcomposer.jsx
+++ postcomposer/src/components/postcomposer.jsx
@@ -1,39 +1,38 @@
 import { useState } from "react";
 import EnforceRules from "./rulesEnforcer";
 import { platformRules } from "./rulesEnforcer";
-
-
+import "../assets/PostComposer.css"
 
 function DeterminePlatform(selectedPlatforms){
     const sum =selectedPlatforms.reduce((a, b) => a + b, 0)
     return sum;
-}
-
+}        
 function ValidatePost(text, selectedPlatforms){
     const errorArray = [];
     if(selectedPlatforms.length<=0){
         errorArray.push("No platforms selected!")
-    }else{
-        if(text.length < 1){
-                        errorArray.push("Not enough characters for"+selectedPlatforms)
-        }
+    }
+    if(text.length < 1){
+                        errorArray.push("Write Something !")
+    }
+    else{
         const platformNumber  = DeterminePlatform(selectedPlatforms);
         // console.log("EnforceRules returned:", EnforceRules(text, platformNumber));
         // console.log("errorArray is now:", errorArray);
         errorArray.push(...EnforceRules(text, platformNumber));
     }
     // console.log(typeof(errorArray))
+
     return errorArray;
-}
-function List(text, selectedPlatforms){
-    const errorArray = ValidatePost(text, selectedPlatforms)
-    const displayList = errorArray.map((error)=><li>{error}</li>);
-    return <ul>{displayList}</ul>
 }
 function PostComposer(){
     const [postText, setPostText] = useState('');
     const [selectedPlatforms, setSelectedPlatforms] = useState([]);
-    const foundErrors = List(postText, selectedPlatforms);
+    const errorArray = ValidatePost(postText, selectedPlatforms);
+    const hasErrors = errorArray.length>0;
+    const isReady = (hasErrors==false) && (postText.length>0);
+    const displayList = errorArray.map((error)=><li>{error}</li>);
+
     // console.log(foundErrors);
     return(
         <div>
@@ -67,17 +66,23 @@ function PostComposer(){
                         }}></input>
                 </div>
                 <div className="userPost">
-                        <textarea rows="20" cols="90" value={postText} onChange={(e)=> setPostText(e.target.value)}></textarea>
+                        <textarea rows="20" cols="90" value={postText} 
+                        onChange={(e)=> setPostText(e.target.value)} 
+                        className={hasErrors && postText.length >= 0 ? 'has-error':'ready'}>
+                        
+                        </textarea>
                 </div>
                 <div className="charactersList">
                         {selectedPlatforms.map((plt)=>{
-                            if(plt == 1){return <p>T:{postText.length}/{platformRules.twitter.maxChars}</p>}
-                            if(plt == 3){return <p>I:{postText.length}/{platformRules.instagram.maxChars}</p>}
-                            if(plt == 5){return <p>F:{postText.length}/{platformRules.facebook.maxChars}</p>}
+                            if(plt == 1){
+                                return <p className={postText.length >= platformRules.twitter.maxChars ? 'overflow':'underflow'}><img className="favicon" src="https://x.com/favicon.ico" alt="X"/>:{postText.length}/{platformRules.twitter.maxChars}</p>
+                            }
+                            if(plt == 3){return <p className={postText.length >= platformRules.instagram.maxChars ? 'overflow':'underflow'}><img className="favicon" src="https://static.cdninstagram.com/rsrc.php/y4/r/QaBlI0OZiks.ico" alt="IG"/>:{postText.length}/{platformRules.instagram.maxChars}</p>}
+                            if(plt == 5){return <p className={postText.length >= platformRules.facebook.maxChars ? 'overflow':'underflow'}><img className="favicon" src="https://static.xx.fbcdn.net/rsrc.php/y1/r/ay1hV6OlegS.ico" alt="FB"/>:{postText.length}/{platformRules.facebook.maxChars}</p>}
                         })}
                 </div>
                 <div className="errors">
-                    {foundErrors}
+                    <ul>{displayList}</ul>
                 </div>
             </form>
         </div>
