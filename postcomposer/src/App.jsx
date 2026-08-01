a44de182a8d6cce39bb97250c1eab16e530b6226
Gurchetan Singh <gurchetan.singh2@icloud.com>
2026-08-01 11:04:13 +0530

    ADD:UI styling to draft component

--- postcomposer/src/App.jsx
+++ postcomposer/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
     <Draft
       drafts={drafts}
       postText={postText}
+      setPostText={setPostText}
       setDraft={setDraft}
       selectedPlatforms={selectedPlatforms}
     />
--- postcomposer/src/components/Draft.jsx
+++ postcomposer/src/components/Draft.jsx
@@ -1,15 +1,17 @@
 import { platformInfo } from "../assets/platformInfo";
+import "../assets/Draft.css"
+import CopyToClipboard from "react-copy-to-clipboard";
 function createDraftObj(postText,selectedPlatforms){
     return {
         "id":crypto.randomUUID(),
         "content":postText,
         "platforms":selectedPlatforms.map((plt)=>{
-            return platformInfo[plt].name;
+            return platformInfo[plt].icon;
         }),
         "timestamp":Date.now()
     }
 }
-function Draft({drafts,setDraft,postText,selectedPlatforms}){
+function Draft({drafts,setDraft,postText,setPostText,selectedPlatforms}){
     return (
         <div>
             <button onClick={()=>{
@@ -18,14 +20,32 @@ function Draft({drafts,setDraft,postText,selectedPlatforms}){
                 setDraft(drafts=>[...drafts,draftOBJ]);
                 console.log(drafts)
             }}>saveDraft</button>
-            <h1>Drafts:</h1>
-            <ul className="Drafts">
+            <div className="Drafts">
                 {drafts.map((obj)=>{
+                        
                         const timestampObj = new Date(obj.timestamp);
                         const dateObj = timestampObj.toLocaleString();
-                        return <li key={obj.id}>post :{obj.content} for {obj.platforms} saved at {dateObj}</li>
+                        return <div className="draftCard"key={obj.id}>
+                            
+                            <div className="platformImages">
+                                <img src={obj.platforms[0]} className={obj.platforms[0]?'':'hidden'}></img>
+                                <img src={obj.platforms[1]} className={obj.platforms[1]?'':'hidden'}></img>
+                                <img src={obj.platforms[2]} className={obj.platforms[2]?'':'hidden'}></img>
+                            </div>
+                            <p className="content">{obj.content}</p>
+                            <p className="timestamp">{dateObj}</p>
+                            <div className="controls">
+                                <button onClick={()=>{
+                                        setPostText(obj.content)
+                                }}>Edit</button>
+                                <button>Delete</button>
+                                <CopyToClipboard text={obj.content}>
+                                <button type="button">Copy to clipboard</button>
+                                </CopyToClipboard>
+                            </div>
+                        </div>
                     })}
-            </ul>
+            </div>
         </div>
     )
   }
